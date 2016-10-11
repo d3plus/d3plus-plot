17cf592bddfedea7ce6ac6ed4396be3d2204eae4
Dave Landry <landry.dave@gmail.com>
2016-10-11 16:32:07 -0400

    updates Line buffer to respect other Shape buffers

--- src/buffers/Line.js
+++ src/buffers/Line.js
@@ -1,16 +1,17 @@
+import {max} from "d3-array";
+
 export default function(data, x, y) {
 
   const s = this._discrete === "x" ? y : x;
 
-  const d = s.domain().slice(),
-        r = s.range().slice();
+  const d = s.domain().slice();
 
-  if (this._discrete === "x") {
-    d.reverse();
-    r.reverse();
-  }
+  if (this._discrete === "x") d.reverse();
 
-  d[1] = s.invert(r[1] + (this._discrete === "x" ? -10 : 10));
+  const vals = data.map(d => d[this._discrete === "x" ? "y" : "x"]);
+  const b = s.invert(s(max(vals)) + (this._discrete === "x" ? -10 : 10));
+
+  if (b > d[1]) d[1] = b;
 
   if (this._discrete === "x") d.reverse();
 
