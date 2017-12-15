45b5aee18aae46cd0c72889cb9017bd21417ad2b
Dave Landry <landry.dave@gmail.com>
2017-12-15 12:22:51 -0500

    fixes diverging stacked bar padding

--- src/buffers/Bar.js
+++ src/buffers/Bar.js
@@ -1,8 +1,17 @@
-import {max} from "d3-array";
+import {max, min, sum} from "d3-array";
+import {nest} from "d3-collection";
 
 import {default as ordinalBuffer} from "./ordinalBuffer";
 
-export default function(data, x, y) {
+/**
+    Adds a buffer to either side of the non-discrete axis.
+    @param {Array} data
+    @param {D3Scale} x
+    @param {D3Scale} y
+    @param {Object} [config]
+    @param {Number} [buffer]
+*/
+export default function(data, x, y, config = {}, buffer = 10) {
 
   const oppScale = this._discrete === "x" ? y : x;
 
@@ -10,10 +19,29 @@ export default function(data, x, y) {
 
   if (this._discrete === "x") oppDomain.reverse();
 
-  const vals = data.map(d => d[this._discrete === "x" ? "y" : "x"]);
-  const b = oppScale.invert(oppScale(max(vals)) + (this._discrete === "x" ? -10 : 10));
+  let negVals, posVals;
+  if (this._stacked) {
+    const groupedData = nest()
+      .key(d => d[this._discrete])
+      .entries(data)
+      .map(d => d.values.map(x => x[this._discrete === "x" ? "y" : "x"]));
+    posVals = groupedData.map(arr => sum(arr.filter(d => d > 0)));
+    negVals = groupedData.map(arr => sum(arr.filter(d => d < 0)));
+  }
+  else {
+    posVals = data.map(d => d[this._discrete === "x" ? "y" : "x"]);
+    negVals = posVals;
+  }
+  let bMax = oppScale(max(posVals));
+  if (bMax !== oppScale(0)) bMax += this._discrete === "x" ? -buffer : buffer;
+  bMax = oppScale.invert(bMax);
 
-  if (b > oppDomain[1]) oppDomain[1] = b;
+  let bMin = oppScale(min(negVals));
+  if (bMin !== oppScale(0)) bMin += this._discrete === "x" ? buffer : -buffer;
+  bMin = oppScale.invert(bMin);
+
+  if (bMax > oppDomain[1]) oppDomain[1] = bMax;
+  if (bMin < oppDomain[0]) oppDomain[0] = bMin;
 
   if (this._discrete === "x") oppDomain.reverse();
 
