58ab7e313f3929a12f95f542df3db00a49e8242a
Dave Landry <landry.dave@gmail.com>
2018-02-27 15:55:01 -0500

    fixes StackedArea JSDoc example

--- src/StackedArea.js
+++ src/StackedArea.js
@@ -5,7 +5,7 @@ import {default as AreaPlot} from "./AreaPlot";
     @extends Area
     @desc Creates a stacked area plot based on an array of data.
     @example <caption>the equivalent of calling:</caption>
-new d3plus.Area()
+new d3plus.AreaPlot()
   .stacked(true)
 */
 export default class StackedArea extends AreaPlot {
