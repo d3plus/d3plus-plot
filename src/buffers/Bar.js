f9b82130b5c4496b3cbdb5a553329e82822228c8
Nick Bond <bond.n@husky.neu.edu>
2018-04-13 10:55:10 -0400

    fixes spelling mistake in isDiscreteX variable name

--- src/buffers/Bar.js
+++ src/buffers/Bar.js
@@ -20,40 +20,40 @@ export default function({data, x, y, x2, y2, buffer = 10}) {
 
   const oppDomain = oppScale.domain().slice();
 
-  const isDiscreetX = this._discrete === "x";
+  const isDiscreteX = this._discrete === "x";
 
-  if (isDiscreetX) oppDomain.reverse();
+  if (isDiscreteX) oppDomain.reverse();
 
   let negVals, posVals;
   if (this._stacked) {
     const groupedData = nest()
       .key(d => d[this._discrete])
       .entries(data)
-      .map(d => d.values.map(x => x[isDiscreetX ? yKey : xKey]));
+      .map(d => d.values.map(x => x[isDiscreteX ? yKey : xKey]));
     posVals = groupedData.map(arr => sum(arr.filter(d => d > 0)));
     negVals = groupedData.map(arr => sum(arr.filter(d => d < 0)));
   }
   else {
-    posVals = data.map(d => d[isDiscreetX ? yKey : xKey]);
+    posVals = data.map(d => d[isDiscreteX ? yKey : xKey]);
     negVals = posVals;
   }
 
   let bMax = oppScale(max(posVals));
-  if (isDiscreetX ? bMax < oppScale(0) : bMax > oppScale(0)) bMax += isDiscreetX ? -buffer : buffer;
+  if (isDiscreteX ? bMax < oppScale(0) : bMax > oppScale(0)) bMax += isDiscreteX ? -buffer : buffer;
   bMax = oppScale.invert(bMax);
 
   let bMin = oppScale(min(negVals));
-  if (isDiscreetX ? bMin > oppScale(0) : bMin < oppScale(0)) bMin += isDiscreetX ? buffer : -buffer;
+  if (isDiscreteX ? bMin > oppScale(0) : bMin < oppScale(0)) bMin += isDiscreteX ? buffer : -buffer;
   bMin = oppScale.invert(bMin);
 
   if (bMax > oppDomain[1]) oppDomain[1] = bMax;
   if (bMin < oppDomain[0]) oppDomain[0] = bMin;
 
-  if (isDiscreetX) oppDomain.reverse();
+  if (isDiscreteX) oppDomain.reverse();
 
   oppScale.domain(oppDomain);
 
-  const discreteScale = isDiscreetX ? x : y;
+  const discreteScale = isDiscreteX ? x : y;
   discreteScale.domain(ordinalBuffer(discreteScale.domain()));
 
   return [x, y];
