bcd27793f92197337bf4b2a6cdd6006d86da2c21
cnavarreteliz <cnavarreteliz@gmail.com>
2018-08-28 11:45:23 -0300

    adds population pyramid class to d3plus

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ export {default as BoxWhisker} from "./src/BoxWhisker";
 export {default as BumpChart} from "./src/BumpChart";
 export {default as LinePlot} from "./src/LinePlot";
 export {default as Plot} from "./src/Plot";
+export {default as Pyramid} from "./src/Pyramid";
 export {default as StackedArea} from "./src/StackedArea";
---
+++ src/Pyramid.js
@@ -0,0 +1,67 @@
+import {assign, constant} from "d3plus-common";
+
+import {default as Plot} from "./Plot";
+
+/**
+    @class Pyramid
+    @extends Plot
+    @desc Creates a population pyramid based on an array of data.
+    @example <caption>the equivalent of calling:</caption>
+new d3plus.Plot()
+  .discrete("y")
+  .shape("Bar")
+  .stacked(true)
+  .xConfig({tickFormat: d => Math.abs(d)})
+*/
+export default class Pyramid extends Plot {
+
+  /**
+      @memberof Pyramid
+      @desc Invoked when creating a new class instance, and overrides any default parameters inherited from Plot.
+      @private
+  */
+  constructor() {
+    super();
+
+    this._discrete = "y";
+    this._shape = constant("Bar");
+    this._stacked = true;
+    this._xConfig = {
+      tickFormat: d => Math.abs(d)
+    };
+
+  }
+
+  /**
+      Extends the draw behavior of the abstract Viz class.
+      @private
+  */
+  _draw(callback) {
+    this._filteredData.map((d, i) => ({
+      __d3plus__: true,
+      data: assign(d, {
+        __d3plusPopulation__:
+          this._groupBy[0](d, i) === "female"
+            ? this._x(d, i) * -1
+            : this._x(d, i)
+      }),
+      i,
+      hci: this._confidence && this._confidence[1] && this._confidence[1](d, i),
+      id: this._ids(d, i)
+        .slice(0, this._drawDepth + 1)
+        .join("_"),
+      lci: this._confidence && this._confidence[0] && this._confidence[0](d, i),
+      shape: this._shape(d, i),
+      x:
+        this._groupBy[0](d, i) === "female"
+          ? this._x(d, i) * -1
+          : this._x(d, i),
+      y: this._y(d, i)
+    }));
+
+    this.x("__d3plusPopulation__");
+    super._draw(callback);
+
+    return this;
+  }
+}
