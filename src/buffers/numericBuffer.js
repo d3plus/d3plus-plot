49ee5ad0df643dce0d8df12b6a298d0fa78d044c
Dave Landry <landry.dave@gmail.com>
2020-07-14 14:46:44 -0400

    fixes diverging y log scales in Plot and adds memory leak guard

--- src/buffers/numericBuffer.js
+++ src/buffers/numericBuffer.js
@@ -2,14 +2,13 @@ import {scaleLog} from "d3-scale";
 
 /** */
 export default function(axis, scale, value, size, range, domain, index, invert) {
-  // console.log("\n");
-  // console.log(invert ? "Y Axis" : "X Axis");
-  // console.log("Index:", index);
-  // console.log("Range", range);
+
   if (invert) {
     domain = domain.slice().reverse();
     range = range.slice().reverse();
   }
+
+  const logMod = Math.abs(Math.log(domain[1] - domain[0]) / 10);
 
   const needsBuffer = () => {
     let tempAxis = axis.copy();
@@ -23,10 +22,10 @@ export default function(axis, scale, value, size, range, domain, index, invert) 
       }
       diverging = d[0] * d[1] < 0;
       if (diverging) {
-        const percentScale = scaleLog().domain([1, Math.abs(d[index])]).range([0, 1]);
+        const percentScale = scaleLog().domain([1e-6, Math.abs(d[index])]).range([0, 1]);
         const leftPercentage = percentScale(Math.abs(d[index ? 0 : 1]));
         const zero = leftPercentage / (leftPercentage + 1) * (r[1] - r[0]);
-        d = (index === 0 ? [d[0], 1] : [1, d[1]]).map(Math.abs);
+        d = (index === 0 ? [d[0], 1e-6] : [1e-6, d[1]]).map(Math.abs);
         r = index === 0 ? [r[0], r[0] + zero] : [r[0] + zero, r[1]];
       }
       tempAxis = scaleLog()
@@ -38,7 +37,7 @@ export default function(axis, scale, value, size, range, domain, index, invert) 
     const tempRange = tempAxis.range();
     let pixelValue;
     if (scale === "log") {
-      pixelValue = !diverging || value < 0 && !index || value > 0 && index ? tempAxis(Math.abs(value)) : tempRange[value < 0 ? 0 : 1];
+      pixelValue = diverging ? tempAxis(Math.abs(value)) : tempAxis(value);
     }
     else pixelValue = tempAxis(value);
 
@@ -50,22 +49,22 @@ export default function(axis, scale, value, size, range, domain, index, invert) 
       if (index === 0) outside = pixelValue - size < tempRange[index];
       else if (index === 1) outside = pixelValue + size > tempRange[index];
     }
-    // console.log("temp", pixelValue, size, tempAxis.domain(), tempRange);
     return outside;
 
   };
 
   if (axis.invert && needsBuffer()) {
     if (scale === "log") {
-      while (needsBuffer()) {
-        const mod = index === 0 ? -1 : 1;
-        domain[index] += domain[index] * 0.1 * mod;
+      let i = 0;
+      while (i < 10 && needsBuffer()) {
+        const mod = (index === 0 ? -1 : 1) * (domain[index] < 0 ? -1 : 1);
+        domain[index] += domain[index] * logMod * mod;
         axis.domain(invert ? domain.slice().reverse() : domain);
+        i++;
       }
     }
     else if (index === 0) {
       const v = axis.invert(axis(value) + size * (invert ? 1 : -1));
-      // console.log("value", v, domain);
       if (v < domain[index]) {
         domain[index] = v;
         axis.domain(invert ? domain.slice().reverse() : domain);
@@ -79,5 +78,6 @@ export default function(axis, scale, value, size, range, domain, index, invert) 
       }
     }
   }
+
   return invert ? domain.reverse() : domain;
 }
